c7d9213046b8df08ae1c729b18d96f855a0b47e4
kwonyul <kw990812@gmail.com>
2024-02-19 16:03:19 +0900

    변수와 타입

---
+++ src/c02/lecture/p02type/C02Integer.java
@@ -0,0 +1,21 @@
+package c02.lecture.p02type;
+
+public class C02Integer {
+    public static void main(String[] args) {
+        // 정수타입
+        // int
+        int a = 5;
+        int b = 10;
+        b = 30;
+        b = 30000000;
+        b = -30000000;
+        b = 0;
+
+        // b = 3.14; x
+
+
+        // long
+
+        // byte, shor
+    }
+}
---
+++ src/c02/sec01/VariableExchangeExample.java
@@ -0,0 +1,24 @@
+package c02.sec01;
+
+public class VariableExchangeExample {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        System.out.println("x:" + x + ", y:" + y);
+
+        int temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x:" + x + ", y:" + y);
+
+        int a = 100;
+        int b = 200;
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+}
