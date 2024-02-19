17a24bb991871705fd6ff565c1446bfe0f6cf24b
kwonyul <kw990812@gmail.com>
2024-02-19 12:41:05 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,11 @@
+public class Main3 {
+
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("저는 권율 입니다");
+        System.out.println("자바 초보 입니다");
+        System.out.println("sout 이용하면 편리");
+
+
+    }
+}
